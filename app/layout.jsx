0aebb7ef56afcdab1f2baad704a15477cab81049
Phil Stubbs <philipstubbs13@gmail.com>
2024-10-08 17:27:23 -0500

    bookit app

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import Header from "../components/Header";
+import "../assets/styles/globals.css";
+import Footer from "../components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthWrapper from "../components/AuthWrapper";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Bookit App | Book a Room",
+  description: "Book a meeting or conference room for your team.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <AuthWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            {children}
+          </main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthWrapper>
+  );
+}
